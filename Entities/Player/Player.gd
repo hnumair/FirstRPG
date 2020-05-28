aed1f9f652d80f1ca3e33e70ab12f102d46824ec
Numair <hussainnumair96@gmail.com>
2020-05-28 22:38:24 +0530

    First Commit

---
+++ Entities/Player/Player.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+
+# Player movement speed
+export var speed = 75
+func _physics_process(delta):
+	# Get player input
+	var direction: Vector2
+	direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	direction.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	# If input is digital, normalize it for diagonala movement
+	if abs(direction.x) == 1 and abs(direction.y) == 1:
+		direction = direction.normalized()
+	
+	# Apply movement
+	var movement = speed * direction * delta
+	move_and_collide(movement) 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
---
+++ README.md
@@ -0,0 +1,19 @@
+# FirstRPG
+A top-down RPG in development using Godot
+
+Progress So Far
+----
+- Main Character
+- Map
+- Terrain
+- Obstacles
+- Controls
+- Collision on all the above
+
+TODO
+----
+- Trees
+- Animation
+- GUI
+- Monsters
+- And much more as I go 
